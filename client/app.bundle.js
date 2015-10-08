f44aa67ab804aa36cd772c19edae8709f638305e
Charles Crame <ccrame@gmail.com>
2015-10-07 19:47:33 -0700

    Fixed for mobile

--- client/app.bundle.js
+++ client/app.bundle.js
@@ -98,26 +98,43 @@ module.exports = function($scope,appFactory){
 },{}],3:[function(require,module,exports){
 module.exports = function($scope, appFactory, $state, $stateParams){
   var articlesRef = appFactory.firebase.child('articles');
-  $scope.articles = [];
+  var commentRef = appFactory.firebase.child('comments');
   $scope.articleData = null;
-  $scope.articleMessage = "Blog Posts"
+  $scope.articles = [];
+  $scope.comments = null;
+  $scope.articleMessage = "Blog Posts";
 
   if($stateParams.article){
     var article = articlesRef.child($stateParams.article);
+    var comments = commentRef.child($stateParams.article);
 
-    article.on("value",function(info){
-      info = info.val();
+    // load article contents
+    article.on("value",function(receivedArticleInfo){
+      receivedArticleInfo = receivedArticleInfo.val();
       article.off();
       appFactory.update($scope,function(scope){
-        scope.articleData = info;
-        scope.articleData.date = 'Published on ' + new Date(info.date).toDateString();
+        scope.articleData = receivedArticleInfo;
+        scope.articleData.date = 'Published on ' + new Date(receivedArticleInfo.date).toDateString();
         scope.articleMessage = "More Blog Posts";
       });
     });
+
+    // load article comments
+    comments.on("value", function(commentData){
+      $scope.comments = [];
+      commentData = commentData.val() || {};
+      comments.off();
+      var keys = Object.keys(commentData);
+      for(var i = keys.length; i >= 0; --i){
+        $scope.comments.push(commentData[keys[i]]);
+      }
+    });
   }
 
+
+  // load list of articles
   articlesRef.on('value',function(articles){
-    articles = articles.val();
+    articles = articles.val() || {};
     articlesRef.off();
     var keys = Object.keys(articles);
     for(var i = keys.length - 1; i >= 0; --i){
@@ -128,6 +145,9 @@ module.exports = function($scope, appFactory, $state, $stateParams){
     }
   });
 
+  $scope.postComment = function(){
+    window.console.log('hello');
+  };
 };
 },{}],4:[function(require,module,exports){
 module.exports = function($scope, appFactory, $state, $stateParams){
