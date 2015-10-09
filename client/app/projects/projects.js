326bb1cea18c9279f38a1f9a83a8365fbcedf7b3
Charles Crame <ccrame@gmail.com>
2015-10-09 13:59:12 -0700

    Changed method to switch between projects in project page

--- client/app/projects/projects.js
+++ client/app/projects/projects.js
@@ -1,8 +1,13 @@
 module.exports = function($scope,appFactory,$state){
-  $scope.selected = [1,0,0];
+  $scope.selected = 0;
+  $scope.projects = ['Linelevel','Political Profiler','Challenge Accepted!'];
+  $scope.projectLinks = ['http://linelevel.herokuapp.com','https://politicalprofiler.herokuapp.com','https://heraapphrr7.herokuapp.com/#!/'];
 
-  $scope.selectProject = function(num){
-    $scope.selected = [0,0,0];
-    $scope.selected[num] = 1;
+  $scope.go = function(direction){
+    if(direction === 'left' && $scope.selected > 0){
+      $scope.selected--;
+    } else if ( direction === 'right' && $scope.selected < $scope.projects.length-1){
+      $scope.selected++;
+    }
   };
 };
