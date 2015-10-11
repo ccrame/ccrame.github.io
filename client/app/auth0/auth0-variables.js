5ef11b03c20a2d24ed4b2daad972613c774a6463
Charles Crame <ccrame@gmail.com>
2015-10-10 23:32:16 -0700

    added proper version of angular-cookies for mobile

--- client/app/auth0/auth0-variables.js
+++ client/app/auth0/auth0-variables.js
@@ -1,3 +1,3 @@
 AUTH0_CLIENT_ID = '2BGFav69I7MyFUW8gzn8p69HWIszklnN'; 
 AUTH0_DOMAIN = 'willowcorp.auth0.com'; 
-AUTH0_CALLBACK_URL = location.href;
+AUTH0_CALLBACK_URL = location.href || 'https://willowcorp.auth0.com/login/callback';
