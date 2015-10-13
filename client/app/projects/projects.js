831c881013afab6fe4c20a24af6416d72f56e837
Charles Crame <ccrame@gmail.com>
2015-10-12 23:20:09 -0700

    Modifications to project page

--- client/app/projects/projects.js
+++ client/app/projects/projects.js
@@ -1,12 +1,28 @@
 module.exports = function($scope,appFactory,$state){
   $scope.selected = 0;
-  $scope.projects = ['Linelevel','Political Profiler','Challenge Accepted!'];
-  $scope.projectLinks = ['http://linelevel.herokuapp.com','https://politicalprofiler.herokuapp.com','https://heraapphrr7.herokuapp.com/#!/'];
 
+  // project data
+  $scope.project = {};
+  $scope.project.title = ['Linelevel','Political Profiler','Challenge Accepted!'];
+
+  $scope.project.link = ['http://linelevel.herokuapp.com',
+                         'https://politicalprofiler.herokuapp.com',
+                         'https://heraapphrr7.herokuapp.com/#!/'];
+
+  $scope.project.description = ['A social media app where artists can stream videos to their fans',
+                                'A site that allows users to compare politicians\' voting histories',
+                                'A web app where users can manage their daily tasks and share collections of tasks'];
+
+  $scope.project.techStack = ['Angular.js, Firebase, Jasmine, WebRTC, Gulp.js',
+                              'GovTrack API, Angular.js, Node.js/Express.js, Gulp.js, Jasmine, Bower',
+                              'Angular.js, MongoDB/Mongoose, Node.js/Express.js, Grunt.js'];
+
+
+  // helper functions
   $scope.go = function(direction){
     if(direction === 'left' && $scope.selected > 0){
       $scope.selected--;
-    } else if ( direction === 'right' && $scope.selected < $scope.projects.length-1){
+    } else if ( direction === 'right' && $scope.selected < $scope.project.title.length-1){
       $scope.selected++;
     }
   };
