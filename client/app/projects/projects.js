4f2eb9012e3230d34a94644241c95c7931b0bb96
Charles Crame <ccrame@gmail.com>
2015-10-13 23:05:30 -0700

    Implemented scrolling images in project page

--- client/app/projects/projects.js
+++ client/app/projects/projects.js
@@ -1,7 +1,12 @@
 module.exports = function($scope,appFactory,$state){
   $scope.selected = 0;
+  $scope.showImageControls = false;
 
+
+  /////////////////////
   // project data
+  /////////////////////
+
   $scope.project = {};
   $scope.project.title = ['Linelevel','Political Profiler','Challenge Accepted!'];
 
@@ -18,12 +23,76 @@ module.exports = function($scope,appFactory,$state){
                               'Angular.js, MongoDB/Mongoose, Node.js/Express.js, Grunt.js'];
 
 
-  // helper functions
-  $scope.go = function(direction){
+  ////////////////////////////////////////////////////////////
+  // helper function to scroll through projects on the screen
+  ////////////////////////////////////////////////////////////
+  $scope.switchProject = function(direction){
     if(direction === 'left' && $scope.selected > 0){
       $scope.selected--;
     } else if ( direction === 'right' && $scope.selected < $scope.project.title.length-1){
       $scope.selected++;
     }
   };
+
+  ///////////////////////////////////////
+  // helpers to hide/show image controls
+  ///////////////////////////////////////
+
+  // track the setTimeout
+  var hideControlTimeout = null;
+
+  var updateControlStatus = function(input){
+    appFactory.update($scope,function(scope){
+      scope.showImageControls = input;
+    });
+  };
+
+  var hide = function(){
+    updateControlStatus(false);
+  };
+
+  $scope.showControls = function(){
+    clearTimeout(hideControlTimeout);
+    updateControlStatus(true);
+  };
+
+  $scope.hideControls = function(){
+    hideControlTimeout = setTimeout(hide,1000);
+  };
+
+  ////////////////////////////////////////////////
+  // helpers to scroll through the project images
+  ////////////////////////////////////////////////
+
+  // shows the number of images per project
+  $scope.imageCount = [4,4,3];
+  $scope.imageScrollState = [0,0,0];
+
+  $scope.scrollImage = function(direction){
+    if(direction === 'left' && $scope.imageScrollState[$scope.selected] > 0){
+      $scope.imageScrollState[$scope.selected]--;
+    } else if (direction === 'right' && $scope.imageScrollState[$scope.selected] < $scope.imageCount[$scope.selected] - 1){
+      $scope.imageScrollState[$scope.selected]++;
+    }
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
